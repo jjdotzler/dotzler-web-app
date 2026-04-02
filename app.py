fcf3bfb9b570745cc16aa18c8407bdd8a3f7036d
jjdotzler <john-dotzler@uiowa.edu>
2026-04-02 14:51:55 -0500

    Edited for web pages

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 # module 11 - Flask Application
 # Mike Colbert 3/31/2026
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 
 @app.route("/")
@@ -20,5 +25,10 @@ def jack():
     return f"Hello {name}, the sum of {x} and {y} is {z}!"
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5002, debug=True)
+    app.run(host="0.0.0.0", port=5003, debug=True)
